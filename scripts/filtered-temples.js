ec3e722963c2072429896f09e6691bccb12fd421
johnanemba <johnanemba@gmail.com>
2025-04-08 19:06:28 +0300

    a

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -12,70 +12,70 @@ document.addEventListener("DOMContentLoaded", () => {
             location: "Salt Lake City, Utah, USA",
             dedicated: "1893-04-06",
             area: 253015,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-35082.jpg"
+            imageUrl: "https://www.churchofjesuschrist.org/imgs/cd4d22a965af18a50f8589bb1b917859d5935209/full/!1920%2C1285/0/default"
         },
         {
             name: "Rome Italy Temple",
             location: "Rome, Italy",
             dedicated: "2019-03-10",
             area: 40000,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-26076.jpg"
+            imageUrl: "https://www.churchofjesuschrist.org/imgs/17e2c70d687fffedfe115197e57fa8f5d1d369bb/full/640%2C/0/default"
         },
         {
-            name: "Nairobi Kenya Temple",
-            location: "Nairobi, Kenya",
-            dedicated: "2024-09-15",
-            area: 17000,
-            imageUrl: "https://www.churchofjesuschrist.org/imgs/93148bc4e2b74c79902baf7567e31a02"
+            name: "tokyo Japan Temple",
+            location: "Tokyo, Japan",
+            dedicated: "2021-01-10",
+            area: 52590,
+            imageUrl: "https://www.churchofjesuschrist.org/imgs/df6b96801c9f11ec99eeeeeeac1ea2207e7c517b/full/640%2C/0/default"
         },
         {
             name: "Los Angeles California Temple",
             location: "Los Angeles, California, USA",
             dedicated: "1956-03-11",
             area: 190614,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/los-angeles-california-temple/los-angeles-california-temple-1243.jpg"
+            imageUrl: "https://newsroom.churchofjesuschrist.org/media/960x540/Los-Angeles-California-Temple2.jpg"
         },
         {
             name: "Payson Utah Temple",
             location: "Payson, Utah, USA",
             dedicated: "2015-06-07",
             area: 96530,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/payson-utah-temple/payson-utah-temple-12841.jpg"
+            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/800x500/payson-utah-temple-daylight-1416668-wallpaper.jpg"
         },
         {
             name: "Accra Ghana Temple",
             location: "Accra, Ghana",
             dedicated: "2004-01-11",
             area: 17142,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple/accra-ghana-temple-258.jpg"
+            imageUrl: "https://www.churchofjesuschrist.org/imgs/ea817531789318cff9d81198cdc39923708b7b79/full/500%2C/0/default"
         },
         {
             name: "Washington D.C. Temple",
             location: "Kensington, Maryland, USA",
             dedicated: "1974-11-19",
             area: 160000,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/washington-dc-temple/washington-dc-temple-1844.jpg"
+            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/washington-d.c.-temple/washington-d.c.-temple-29515-thumb.jpg"
         },
         {
             name: "Brigham City Utah Temple",
             location: "Brigham City, Utah, USA",
             dedicated: "2012-09-23",
             area: 70000,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/brigham-city-utah-temple/brigham-city-utah-temple-9334.jpg"
+            imageUrl: "https://lh3.googleusercontent.com/gps-cs-s/AB5caB--YdZgEWyqOAiNH1c3HwfsOghC3cw4hh_2PyMZz7BhUtOOZ9yfvF9HjVwGG5oz-U0MpgZJ-uffpEFbVRKbLgbGg2Sb_yib0Yq9T7cYS2HwP48F8S-t8lJO2-azUJoL5xxcbSEa=s1360-w1360-h1020"
         },
         {
             name: "Paris France Temple",
             location: "Le Chesnay, France",
             dedicated: "2017-05-21",
             area: 44000,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-13238.jpg"
+            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/paris-france/800x500/paris-france-temple-exterior-1905503.jpg"
         },
         {
             name: "Johannesburg South Africa Temple",
             location: "Johannesburg, South Africa",
             dedicated: "1985-08-24",
             area: 19000,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/johannesburg-south-africa-temple/johannesburg-south-africa-temple-520.jpg"
+            imageUrl: "https://www.churchofjesuschrist.org/media/image/johannesburg-south-africa-temple-lds-e44b3c8?lang=eng&collectionId=644db7259bef4743a8530dcb551ba116"
         }
     ];
 
